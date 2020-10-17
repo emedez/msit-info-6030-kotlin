53bf4bb5e9bb4bee3bf15a1af8a31f59d52ad8ef
Elmer Medez <emedez@gmail.com>
2020-10-17 11:28:11 +0800

    Add demos on type checks and automatic casts

---
+++ type_check_01.kt
@@ -0,0 +1,26 @@
+/* 
+Type checks and automatic casts
+
+The is operator checks if an expression is an instance of a type. If an immutable local variable or property is checked for a specific type, there's no need to cast it explicitly.
+*/
+
+fun getStringLength(obj: Any): Int? {
+    if (obj is String) {
+        // `obj` is automatically cast to `String` in this branch
+        return obj.length
+    }
+
+    // `obj` is still of type `Any` outside of the type-checked branch
+    return null
+}
+
+
+fun main() {
+    fun printLength(obj: Any) {
+        println("'$obj' string length is ${getStringLength(obj) ?: "... err, not a string"} ")
+    }
+    printLength("Incomprehensibilities")
+    printLength(1000)
+    printLength(listOf(Any()))
+}
+
---
+++ type_check_02.kt
@@ -0,0 +1,21 @@
+/* 
+Type checks and automatic casts
+*/
+
+fun getStringLength(obj: Any): Int? {
+    if (obj !is String) return null
+
+    // `obj` is automatically cast to `String` in this branch
+    return obj.length
+}
+
+
+fun main() {
+    fun printLength(obj: Any) {
+        println("'$obj' string length is ${getStringLength(obj) ?: "... err, not a string"} ")
+    }
+    printLength("Incomprehensibilities")
+    printLength(1000)
+    printLength(listOf(Any()))
+}
+
