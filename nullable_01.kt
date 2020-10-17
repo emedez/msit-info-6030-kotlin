ad807631b883bfcfde9745d64aa19b10b21f1504
Elmer Medez <emedez@gmail.com>
2020-10-17 11:15:07 +0800

    Add demos on nullable values and null checks

---
+++ nullable_01.kt
@@ -0,0 +1,29 @@
+/* Nullable values and null checks */
+
+
+// A reference must be explicitly marked as nullable when null value is possible. 
+fun parseInt(str: String): Int? {
+    return str.toIntOrNull()
+}
+
+fun printProduct(arg1: String, arg2: String) {
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+
+    // Using `x * y` yields error because they may hold nulls.
+    if (x != null && y != null) {
+        // x and y are automatically cast to non-nullable after null check
+        println(x * y)
+    }
+    else {
+        println("'$arg1' or '$arg2' is not a number")
+    }    
+}
+
+
+fun main() {
+    printProduct("6", "7")
+    printProduct("a", "7")
+    printProduct("a", "b")
+}
+
