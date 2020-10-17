27b0db31f3be66b2541299581c444fd580766ad7
Elmer Medez <emedez@gmail.com>
2020-10-17 10:16:58 +0800

    Add demos on variables

---
+++ variables_01.kt
@@ -0,0 +1,10 @@
+/* Read-only local variables are defined using the keyword val. They can be assigned a value only once. */
+
+fun main() {
+    val a: Int = 1  // immediate assignment
+    val b = 2   // `Int` type is inferred
+    val c: Int  // Type required when no initializer is provided
+    c = 3       // deferred assignment
+    println("a = $a, b = $b, c = $c")
+}
+
---
+++ variables_02.kt
@@ -0,0 +1,8 @@
+/* Variables that can be reassigned use the var keyword */
+
+fun main() {
+    var x = 5 // `Int` type is inferred
+    x += 1
+    println("x = $x")
+}
+
